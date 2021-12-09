1119a80ffdd00e01c49c4aa41dc99d2f4c850941
ThomasTeggin <tomteggingmail.com>
2021-12-09 15:39:44 +0100

    Template card statique

--- views/lovers.php
+++ views/lovers.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,9 +8,31 @@
     <title>Lovers</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 </head>
+
 <body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="assets/js/script.js"></script>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card my-2">
+                    <img src="../assets/img/pexels-andrea-piacquadio-762020.jpg" class="card-img-top" alt="image_de_profil">
+                    <div class="card-body">
+                        <div class="card-text">
+                            <p class="float-end me-4 mt-3"><i class="fas fa-map-pin"></i> 75008</p>
+                            <h5 class="fs-4">Chloé Dupond</h5>
+                            <h5 class="text-muted">23 ans</h5>
+                            <hr class="my-2" />
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, molestias.</p>
+                            <div class="text-center"><button type="button" class="btn btn-success fs-5 rounded-pill"><i class="fas fa-heart"></i></button></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
 </body>
+
 </html>
