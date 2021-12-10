a9d58dd7fc276618f2d44678fb4e4ade2ab26772
loicksauvageee <loicksauvage.pro@gmail.com>
2021-12-10 11:22:52 +0100

    user php prêt a test

---
+++ controllers/user_controller.php
@@ -0,0 +1,22 @@
+<?php
+if (isset($_COOKIE["lastname"], $_COOKIE["firstname"], $_COOKIE["age"], $_COOKIE["gender"], $_COOKIE["zipcode"], $_COOKIE["research_type"])&& !empty($_COOKIE["lastname"]) && !empty($_COOKIE["firstname"]) && !empty($_COOKIE["age"]) && !empty($_COOKIE["gender"]) && !empty($_COOKIE["zipcode"]) && !empty($_COOKIE["mail"]) && !empty($_COOKIE["research_type"])) {
+
+   $lastname = $_COOKIE["lastname"];
+   $firstname = $_COOKIE["firstname"];
+   $age = $_COOKIE["age"];
+   $gender = $_COOKIE["gender"];
+   $zipcode = $_COOKIE["zipcode"];
+   $research_type = $_COOKIE["reseach_type"];
+
+
+    
+}else {
+    header('location: ../index.php');
+}
+
+
+if ($_POST["submit"] == "delete") {
+    setcookie('lastname','', time() -3600);
+    header('location: ../index.php');
+}
+?>
--- views/user.php
+++ views/user.php
@@ -1,15 +1,71 @@
+<?php
+include("../controllers/user_controller.php");
+?>
+
+
+
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="assets/js/script.js"></script>
-</body>
+<html lang="en">
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+      <!--LA DATA-->
+    <div class="container-fluid text-center pt-4 bg-dark text-white h3 ">
+        <div>
+            <p class="h1 "> <span class="text-danger">L</span>es <span class="text-danger">P</span>aramètres <span class="text-danger">D</span>u <span class="text-danger">P</span>rofil</p>
+        </div>
+      <div class="row pt-4">
+        <div class="col">Nom : </div>
+        <div class="col"><?= $lastname;?></div>
+        <div class="col text-muted">/modifier</div>
+      </div>
+
+      <div class="row">
+        <div class="col">Prénom:</div>
+        <div class="col"><?= $firstname;?></div>
+        <div class="col text-muted">/modifier</div>
+      </div>
+
+      <div class="row">
+        <div class="col">Genre :</div>
+        <div class="col"><?= $age;?></div>
+        <div class="col text-muted">/modifier</div>
+      </div>
+
+      <div class="row">
+        <div class="col">Code postal :</div>
+        <div class="col"><?= $gender;?></div>
+        <div class="col text-muted">/modifier</div>
+</div>
+      <div class="row">
+        <div class="col">Code postal :</div>
+        <div class="col"><?= $zipcode;?></div>
+        <div class="col text-muted">/modifier</div>
+</div>
+
+      <div class="row">
+        <div class="col">Adresse mail :</div>
+        <div class="col"><?= $research_type;?></div>
+        <div class="col text-muted">/modifier</div>
+      </div>
+
+	  <form action="../controllers/user_controller.php" method="POST">
+      <div class="text-center bg-dark p-4">
+        <button name="submit" value="delete" type="submit" class="btn btn-outline-danger btn-lg">EFFACER</button>
+
+    <button type="button" class="btn btn-secondary btn-lg">MEETIC</button>
+    </div>
+	</form>
+    </div>
+    <!--LA DATA-->
+
+  </body>
 </html>
