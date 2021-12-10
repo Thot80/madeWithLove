f202722bb7543d2d9bdd6a8b9465b76f53076b8c
Jeremy Kessous <jeremykessous@yahoo.fr>
2021-12-10 16:44:30 +0100

    Rose toggle when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
-let hearts = document.getElementsByClassName('heart');
+let hearts = document.getElementsByClassName("hearts");
 
-hearts.forEach( element => element.addEventListener("click", function(){
-    element.classList.toggle("active");
-})
-)
+console.log(hearts);
+
+for (let i = 0; i < hearts.length; i++ ){
+    hearts[i].addEventListener("click", function(){
+        hearts[i].classList.toggle("active");
+    })
+}
