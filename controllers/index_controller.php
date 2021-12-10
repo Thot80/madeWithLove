d770b912e09f5bd3f993ecaca3111471b6f6e8d0
Jeremy Kessous <jeremykessous@yahoo.fr>
2021-12-10 14:36:51 +0100

    changement de nom de domaine

--- controllers/index_controller.php
+++ controllers/index_controller.php
@@ -6,13 +6,13 @@ if(isset($_POST["lastname"], $_POST["firstname"], $_POST["age"], $_POST["gender"
 $_POST["research_type"]) && !empty($_POST["lastname"]) && !empty($_POST["firstname"]) && !empty($_POST["age"]) && !empty($_POST["gender"]) && !empty($_POST["zipcode"]) && !empty($_POST["mail"]) && !empty($_POST["research_type"])){
 
     // Les données sont bonnes, on les enregistre dans un cookie et on redirige vers la page user.php
-    setcookie("lastname", $_POST["lastname"], time() + 86400, '/', 'madeWithLove');
-    setcookie("firstname", $_POST["firstname"], time() + 86400, '/', 'madeWithLove');
-    setcookie("age", $_POST["age"], time() + 86400, '/', 'madeWithLove');
-    setcookie("gender", $_POST["gender"], time() + 86400, '/', 'madeWithLove');
-    setcookie("zipcode", $_POST["zipcode"], time() + 86400, '/', 'madeWithLove');
-    setcookie("mail", $_POST["mail"], time() + 86400, '/', 'madeWithLove');
-    setcookie("research_type", $_POST["research_type"], time() + 86400, '/', 'madeWithLove');
+    setcookie("lastname", $_POST["lastname"], time() + 86400, '/', 'madewithlove.fr');
+    setcookie("firstname", $_POST["firstname"], time() + 86400, '/', 'madewithlove.fr');
+    setcookie("age", $_POST["age"], time() + 86400, '/', 'madewithlove.fr');
+    setcookie("gender", $_POST["gender"], time() + 86400, '/', 'madewithlove.fr');
+    setcookie("zipcode", $_POST["zipcode"], time() + 86400, '/', 'madewithlove.fr');
+    setcookie("mail", $_POST["mail"], time() + 86400, '/', 'madewithlove.fr');
+    setcookie("research_type", $_POST["research_type"], time() + 86400, '/', 'madewithlove.fr');
 
     header('Location: ../views/user.php');
 }
